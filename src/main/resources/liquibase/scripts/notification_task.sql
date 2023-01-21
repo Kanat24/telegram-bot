2e6ba7b617dc49d2a50e0480a58aec7ffc1d5a67
Sajer24 <sajer.kanat1992@gmail.com>
2023-01-21 22:47:11 +0300

    коммит

--- src/main/resources/liquibase/scripts/notification_task.sql
+++ src/main/resources/liquibase/scripts/notification_task.sql
@@ -1,8 +1,10 @@
 -- liquibase formatted sql
 
 -- changeset rudnev:1
- CREATE TABLE notification_task(
-    id        SERIAL PRIMARY KEY,
-    text      Text,
-    data_time TIMESTAMP
+CREATE TABLE notification_task
+(
+    id                   SERIAL PRIMARY KEY,
+    message              Text,
+    notification_chat_id SERIAL,
+    local_date_time      TIMESTAMP
 )
